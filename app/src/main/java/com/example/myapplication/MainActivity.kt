f2932b2cf473bd3ea8baa69d8dbf78d68a18b998
nisarg.k.amin@gmail.com <nisarg.k.amin@gmail.com>
2022-08-23 14:25:46 -0400

    First commit..

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test()
+    }
 
+
+    fun test() {
+        print("Testing new function")
     }
 }
